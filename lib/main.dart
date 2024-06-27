91d641bf9ef271056f43a04fa5799d678152c0a5
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-06-27 12:42:17 +0500

    cart screen qilindi full emas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_competition/views/screens/cart_scren_purchase.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: CartScreenPurchase());
   }
 }
---
+++ lib/views/widgets/container_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class ContainerWidget extends StatelessWidget {
+  final String firstText;
+  final String secondText;
+  final bool showImage;
+  final Widget? additionalWidget;
+
+  ContainerWidget({
+    required this.firstText,
+    required this.secondText,
+    this.showImage = true,
+    this.additionalWidget,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        width: double.infinity,
+        height: 100,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Icon(Icons.shopping_cart_outlined, size: 35),
+                  const SizedBox(width: 20),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        firstText,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22),
+                      ),
+                      Text(
+                        secondText,
+                        style:
+                            const TextStyle(color: Colors.grey, fontSize: 18),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              if (showImage)
+                Image.asset("assets/icons/forward.png", width: 35)
+              else
+                additionalWidget ?? Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
